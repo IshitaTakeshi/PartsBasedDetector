230402cb8641b556e16ce01b75aeaa3b915f702e
sonicair <ishitah.takeshi@gmail.com>
2016-02-07 01:28:37 +0900

    Get rid of environment-dependent contents

--- include/estimator.h
+++ include/estimator.h
@@ -8,20 +8,21 @@ extern "C" {
 
 
 typedef struct point_t {
-  unsigned long x;
-  unsigned long y;
+  unsigned int x;
+  unsigned int y;
 } point_t;
 
 
 typedef struct candidate_t {
-  unsigned long size;
-  point_t **parts;
-  float *confidence;
+  unsigned int size;  //the number of parts
+  point_t **parts;  // the coordinates of the parts
+  float *confidence;  //the confidence scores of the parts
 } candidate_t;
 
 
+//structure to hold a candidate array
 typedef struct candidates_t {
-  unsigned long size;
+  unsigned int size;
   candidate_t **candidates;
 } candidates_t;
 
@@ -29,8 +30,10 @@ typedef struct candidates_t {
 void* create_estimator(char filename[]);
 candidates_t *estimate(void *estimator, char filename[]);
 void destroy_estimator(void *estimator);
+void print_candidate(candidate_t *candidate);
 void free_candidates(candidates_t* candidates);
 void test_calling();
+void test();
 
 
 #ifdef __cplusplus
