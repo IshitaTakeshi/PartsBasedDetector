432c8ce7c31e24a39d5fd4ef85fd9e3432cdcf45
Ishita Takeshi <ishitah.takeshi@gmail.com>
2016-04-06 14:24:53 +0900

    Delete printing, show usage if arguments are not enough

--- src/pose_estimator_demo.c
+++ src/pose_estimator_demo.c
@@ -1,17 +1,20 @@
-#include "estimator.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "estimator.h"
+
 
 int main(int argc, char** argv) {
+    //test();
+
+    if(argc < 3) {
+        printf("Usage: PartsBasedDetector1 model_file image_file\n");
+        exit(0);
+    }
+
     void* estimator = create_estimator(argv[1]);
     candidates_t* candidates = estimate(estimator, argv[2]);
-
-    for(unsigned int i = 0; i < candidates->candidates[0]->size; i++) {
-        printf("x: %4lu  y: %4lu  confidence: %.4f \n",
-                candidates->candidates[0]->parts[i]->x,
-                candidates->candidates[0]->parts[i]->y,
-                candidates->candidates[0]->confidence[i]);
-    }
-    printf("\n");
+    print_candidate(candidates->candidates[0]);
 
     destroy_estimator(estimator);
     free_candidates(candidates);
